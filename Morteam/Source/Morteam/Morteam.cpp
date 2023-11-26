5055f50c661ead9a606a27a946cdc3e508fb7545
Aleksander Mort <aleksandermort@gmail.com>
2023-11-26 21:58:14 +0300

    ADD

---
+++ Morteam/Source/Morteam/Morteam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Morteam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Morteam, "Morteam" );
